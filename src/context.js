e428032ce7eed552d25f3e9716226c8b057887c9
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-04-21 16:47:51 -0700

    add global context

--- src/context.js
+++ src/context.js
@@ -5,5 +5,8 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
     return <AppContext.Provider value="hello">{children}</AppContext.Provider>
 }
+export const useGlobalContext = () => {
+    return useContext(AppContext)
+}
 
 export { AppContext, AppProvider }
